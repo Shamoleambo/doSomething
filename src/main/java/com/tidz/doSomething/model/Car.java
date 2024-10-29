7e40b8874bc910d68d600ae73116599918f29035
Tiago Gomes <tidgomes@gmail.com>
2024-10-29 12:25:08 -0300

    create the model enum and car model

---
+++ src/main/java/com/tidz/doSomething/model/Car.java
@@ -0,0 +1,73 @@
+package com.tidz.doSomething.model;
+
+import jakarta.persistence.*;
+
+import java.time.Year;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "year", nullable = false)
+    private Year year;
+    @Column(name = "model", nullable = false)
+    private CarModel model;
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    public Car() {
+
+    }
+
+    public Car(String name, Year year, CarModel model, Double price) {
+        this.name = name;
+        this.year = year;
+        this.model = model;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public CarModel getModel() {
+        return model;
+    }
+
+    public void setModel(CarModel model) {
+        this.model = model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/tidz/doSomething/model/CarModel.java
@@ -0,0 +1,5 @@
+package com.tidz.doSomething.model;
+
+public enum CarModel {
+    SEDAN, HATCH, WAGON, SUV, TRUCK, VAN, COUPE;
+}
